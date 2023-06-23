6bb2d70bc2a04f501722e95677af804371e8c265
rodrigocloureiro <rodrigocostaloureiro563@gmail.com>
2023-06-23 18:24:44 -0300

    Lógica do cálculo implementada

--- App.js
+++ App.js
@@ -1,9 +1,36 @@
-import { StyleSheet, View, SafeAreaView } from "react-native";
+import { StyleSheet, View, SafeAreaView, Keyboard } from "react-native";
 import { Text, TextInput, Button } from "react-native-paper";
 import Card from "./components/Card";
 import { SimpleLineIcons } from '@expo/vector-icons';
+import { useState } from "react";
 
 export default function App() {
+  const [ weight, setWeight ] = useState('');
+  const [ height, setHeight ] = useState('');
+  const [ imc, setImc ] = useState(0);
+
+  const handleWeight = (value) => {
+    if(isNaN(value) || value === '') {
+      setWeight('');
+    } else {
+      setWeight(value);
+    }
+  };
+
+  const handleHeight = (value) => {
+    if(isNaN(value) || value === '') {
+      setHeight('');
+    } else {
+      setHeight(value);
+    }
+  };
+
+  const handleCalculate = () => {
+    const imc = (weight / (height*height));
+    setImc(parseFloat(imc.toFixed(2)));
+    Keyboard.dismiss();
+  };
+
   return (
     <SafeAreaView style={styles.app}>
       <View style={styles.container}>
@@ -23,6 +50,8 @@ export default function App() {
           activeUnderlineColor='#ccc'
           underlineColor='#ccc'
           keyboardType='numeric'
+          value={String(weight)}
+          onChangeText={value => handleWeight(value)}
         />
         <TextInput
           textColor='#fff'
@@ -33,38 +62,44 @@ export default function App() {
           activeUnderlineColor='#ccc'
           underlineColor='#ccc'
           keyboardType='numeric'
+          value={String(height)}
+          onChangeText={value => handleHeight(value)}
         />
         <Button
           style={styles.button}
           mode='contained'
           buttonColor='#2587a8'
           textColor='#ffffffde'
-          onPress={() => alert('Clicou!')}
+          onPress={handleCalculate}
         >
           Calcular
         </Button>
         <View style={styles.cards}>
           <Card
             title='Magreza'
-            text='O IMC está menor que 18.5'
+            text='Está menor que 18.5'
+            result={imc <= 18.5 ? imc : 0}
           >
             <SimpleLineIcons name="dislike" size={40} color="#000" />
           </Card>
           <Card
             title='Normal'
-            text='O IMC está entre 18.5 e 24.9'
+            text='Está entre 18.5 e 24.9'
+            result={imc >= 18.5 && imc <= 24.9 ? imc : 0}
           >
             <SimpleLineIcons name="like" size={40} color="#000" />
           </Card>
           <Card
             title='Sobrepeso'
-            text='O IMC está entre 25 e 29.9'
+            text='Está entre 25 e 29.9'
+            result={imc >= 25 && imc <= 29.9 ? imc : 0}
           >
             <SimpleLineIcons name="dislike" size={40} color="#000" />
           </Card>
           <Card
             title='Obesidade'
-            text='O IMC está maior que 30'
+            text='Está maior que 30'
+            result={imc >= 30 ? imc : 0}
           >
             <SimpleLineIcons name="dislike" size={40} color="#000" />
           </Card>
--- components/Card.js
+++ components/Card.js
@@ -1,19 +1,25 @@
 import { StyleSheet, View } from 'react-native';
 import { Text } from 'react-native-paper';
 
-export default function Card({ children, title, text }) {
+export default function Card({ children, title, text, result }) {
   return (
     <View style={styles.card}>
       {children}
       <Text variant='titleLarge' style={styles.title}>{title}</Text>
       <Text variant='bodyMedium' style={styles.text}>{text}</Text>
+      <Text
+        variant='bodyMedium'
+        style={[styles.result, result <= 0 && {opacity: 0}]}
+      >
+        {result}
+      </Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   card: {
-    maxWidth: '48%',
+    width: '48%',
     backgroundColor: '#5c5c5c',
     alignItems: 'center',
     padding: 10,
@@ -26,5 +32,10 @@ const styles = StyleSheet.create({
   },
   text: {
     color: '#ffffffde',
+  },
+  result: {
+    color: '#ffffffde',
+    textDecorationLine: 'underline',
+    fontWeight: '500',
   },
 });
